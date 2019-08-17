d325a9b625c12be8845cb17b5f68dcf3516d9d76
Ajesh DS <ajeshd17@gmail.com>
2019-08-17 21:49:54 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../styles/App.css';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Expensify</p>
+			</div>
+		)
+	}
+}
+
+export default App;
