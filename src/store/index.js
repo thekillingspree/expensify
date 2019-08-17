c60b69e71d593a45afe19f22173ba53f2f1d8b97
Ajesh DS <ajeshd17@gmail.com>
2019-08-17 22:18:37 +0530

    Use provided storage wrapper

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = persistReducer({
-    storage: localStorage,
+    storage,
     key: 'root'
 }, reducers);
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
+
 export default store;
 export const persistor = persistStore(store);
