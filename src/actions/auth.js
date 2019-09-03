c311c07c3e5b7c25df727bbc662d968a1ff91049
Ajesh DS <ajeshd17@gmail.com>
2019-09-03 09:21:52 +0530

    Add Logout and Merge Login and Signup calls

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,13 @@
-import axios from 'axios';
-import { API, SIGN_UP, AUTH_ERROR } from '../constants';
+import axios from '../utils/axios';
+import { API, SIGN_UP, AUTH_ERROR, LOGIN, LOGOUT } from '../constants';
 
 const signUp = user => ({
     type: SIGN_UP,
+    user
+});
+
+const login = user => ({
+    type: LOGIN,
     user
 });
 
@@ -11,13 +16,23 @@ const authError = error => ({
     error
 });
 
+export const logout = () => {
+    axios.defaults.headers['Authorization'] = null;
+    return {
+        type: LOGOUT
+    }
+}
 
-export const signUpCall = user => {
+export const authCall = (user, type) => {
     return async dispatch => {
         try {
-            const {data} = await axios.post(`${API}/users/signup`, user);
-            console.log(data);
-            return dispatch(signUp({...data.user, token: data.token}));
+            const {data} = await axios.post(`/users/${type}`, user);
+            const userData = {...data.user, token: data.token};
+            axios.defaults.headers['Authorization'] = data.token;
+            if (type === 'signup') {
+                return dispatch(signUp(userData));
+            }
+            return dispatch(login(userData));
         } catch (error) {
             console.log(error.response.data);
             return error.response.data;
