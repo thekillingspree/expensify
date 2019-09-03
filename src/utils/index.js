9e0f78d9854ce43e54ba33f6d4970b94b8b2892e
Ajesh DS <ajeshd17@gmail.com>
2019-09-03 10:22:39 +0530

    Check email regex

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,7 @@
 export * from './api';
+
+export const checkEmail = email => {
+    // source: Chromium.org
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
