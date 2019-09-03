1e261551688ed1fe591aa4f40e98cfc0a56a8855
Ajesh DS <ajeshd17@gmail.com>
2019-09-03 09:22:10 +0530

    More constants

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,3 +1,4 @@
 export const SIGN_UP = "SIGN_UP";
 export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
 export const AUTH_ERROR = "AUTH_ERROR";
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 export * from './actionTypes';
+export * from './defStates';
 export const API = 'https://expensi-fy.herokuapp.com';
