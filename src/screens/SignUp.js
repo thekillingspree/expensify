438bb0c637c7ec354f18d0950fc872fb622ffe88
Ajesh DS <ajeshd17@gmail.com>
2019-09-03 10:21:05 +0530

    Improved SignUp Page

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -7,6 +7,7 @@ import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import ErrorSnackbar from '../components/ErrorSnackbar';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -15,7 +16,8 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle'
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { connect } from 'react-redux';
-import { signUpCall } from '../actions';
+import { authCall } from '../actions';
+import { checkEmail } from '../utils';
 
 class SignUp extends Component {
 
@@ -26,33 +28,50 @@ class SignUp extends Component {
         currency: '',
         password: '',
         error: null,
+        cerror: null,
         loading: false
     }
+
+
+    constructor(props) {
+        super(props);
+        this.emailLabel = React.createRef();
+        this.passwordLabel = React.createRef();
+    }
+
+    componentDidMount() {
+        this.setState({
+            emailWidth: this.emailLabel.current.offsetWidth,
+            passWidth: this.passwordLabel.current.offsetWidth,
+        });
+    }
+
 
     onChange = event => {
         let {name, value, type} = event.target;
         value = type === 'checkbox' ? event.target.checked : value;
-        console.log(name, value)
-        this.setState({[name]: value});
+        this.setState({[name]: value, error: null, cerror: null});
     }
 
     formSubmit = async event => {
         event.preventDefault(); 
         const {fname, lname, email, currency, password} = this.state;
         const name = fname + ' ' + lname;
-        if (password.length < 6) {
-            return this.setState({error: 'Password must be at least 6 characters long.'});
+        if (password.length < 6 || !checkEmail(email)) {
+            return ;
         }
         if (!currency) {
-            return this.setState({error: 'Please select currency.'});
+            return this.setState({cerror: 'Please select currency.'});
         }
         this.setState({loading: true});
-        const { error } = await this.props.signUpCall({name, email, currency, password});
+        const { error } = await this.props.authCall({name, email, currency, password}, 'signup');
         if (error) {
             if (error.code === 11000) {
                 this.setState({error: 'This email is already associated with an account. Please Login.'});
             } else if (error.errors.email) {
                 this.setState({error: 'Please provide a valid email.'});
+            } else {
+                this.setState({error: 'There was an error while signing you up.'});
             }
         }
         this.setState({loading: false});
@@ -63,7 +82,7 @@ class SignUp extends Component {
     }
 
     render() {
-        const {fname, currency, lname, email, password, error, loading} = this.state;
+        const {fname, currency, lname, email, password, error, cerror, loading, emailWidth, passWidth} = this.state;
         return (
             <div className="auth-form">
                 <Dialog open={loading}>
@@ -81,9 +100,36 @@ class SignUp extends Component {
                 <form onSubmit={this.formSubmit}>
                     <TextField className="auth-input" variant="outlined" name="fname" id="fname" value={fname} required label="First Name" placeholder="Eg. Sam" onChange={this.onChange}/>
                     <TextField className="auth-input" variant="outlined" name="lname" id="lname" value={lname} required label="Last Name" placeholder="Eg. Smith" onChange={this.onChange}/>
-                    <TextField className="auth-input" variant="outlined" type="email" name="email" id="email" value={email} label="Email" required placeholder="Eg. samsmith@email.com" onChange={this.onChange}/>
-                    <TextField className="auth-input" variant="outlined" type="password" name="password" id="password" value={password} label="Password" required placeholder="At least 6 characters" error={password.length > 0 && password.length < 6} pattern=".{6,}" title="6 characters minimum" onChange={this.onChange}/>
-                    <FormControl variant="outlined" className="auth-input">
+                    
+                    <FormControl className="auth-input" variant="outlined" error={email.length > 0 && !checkEmail(email)}>
+                        <InputLabel ref={this.emailLabel} htmlFor="email">Email *</InputLabel>
+                        <OutlinedInput
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        value={email} 
+                        labelWidth={emailWidth}
+                        required placeholder="Eg. samsmith@email.com" 
+                        onChange={this.onChange}
+                        />
+                        <FormHelperText>Please Provide a Valid Email</FormHelperText>
+                    </FormControl>
+                    <FormControl className="auth-input" variant="outlined" onBlur={this.setBlur} error={password.length > 0 && password.length < 6}>
+                        <InputLabel ref={this.passwordLabel} htmlFor="password">Password *</InputLabel>
+                        <OutlinedInput
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        value={password} 
+                        labelWidth={passWidth}
+                        required placeholder="Your password" 
+                        onChange={this.onChange}
+                        />
+                        <FormHelperText>Password must be at-least 6 characters long.</FormHelperText>
+                    </FormControl>
+
+
+                    <FormControl variant="outlined" className="auth-input" error={!!cerror}>
                         <InputLabel htmlFor="outlined-age-simple">
                             Currency *
                         </InputLabel>
@@ -98,6 +144,7 @@ class SignUp extends Component {
                             <MenuItem value="btc">BTC ( ₿ )</MenuItem>
                             <MenuItem value="eth">ETH ( Ξ )</MenuItem>
                         </Select>
+                        <FormHelperText>{cerror}</FormHelperText>
                     </FormControl>
                     <input type="submit" value="Sign Up" />
                 </form>
@@ -106,4 +153,4 @@ class SignUp extends Component {
     }
 }
 
-export default connect(null, {signUpCall})(SignUp);
+export default connect(null, {authCall})(SignUp);
