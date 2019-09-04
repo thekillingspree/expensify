d56f9ad4c8ab591280f16953cb33cacb41b7b651
Ajesh DS <ajeshd17@gmail.com>
2019-09-04 20:39:27 +0530

    Add Request Interceptor

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,10 +5,14 @@ import store from '../store';
 
 const instance = axios.create({
     baseURL: API,
-    headers: {
-        contentType: 'application/json',
-        accept: 'application/json',
-    }
 });
+
+
+instance.interceptors.request.use((config) => {
+    const token = store.getState().user.token;
+    if (token) 
+        return {...config, headers: {...config.headers, Authorization: `Bearer ${token}`}}
+    return config;
+})
 
 export default instance;
